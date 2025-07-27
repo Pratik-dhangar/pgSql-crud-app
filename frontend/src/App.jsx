7b1757bfec6b1f22d995bdac47650ece221226e6
Pratik-dhangar <dhangarpratik5077@gmail.com>
2025-07-27 22:00:54 +0530

    day2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import "./App.css";
 import ModelForm from "./components/ModelForm";
 import Navbar from "./components/Navbar";
-import TableList from "./components/Tablelist";
+import TableList from "./components/TableList";
 
 function App() {
 
-  const[isOpen, setIsOpen]= useState(flase);
+  const[isOpen, setIsOpen]= useState(false);
+  const[modelMode, setModelMode]= useState('add');
+
+const handleOpen =(mode)=>{
+  setIsOpen(true)
+}
+
+
+const handleSubmit =()=>{
+  if(modelMode === 'add'){
+    console.log('modal mode add');
+  } else{
+    console.log('modal mode Edit');
+  }
+}
 
   return (
     <>
--- frontend/src/components/ModelForm.jsx
+++ frontend/src/components/ModelForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ModelForm = () => {
   return (
     <div>
-      
+      <h1>he</h1>
     </div>
   )
 }
