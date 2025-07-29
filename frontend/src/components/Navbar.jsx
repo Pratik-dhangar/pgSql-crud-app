05a0517d8bb1df77ef868bbf779150a65d0dfa3c
Pratik Dhangar <161105677+Pratik-dhangar@users.noreply.github.com>
2025-07-29 19:19:59 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 export default function Navbar({ onOpen, onSearch}) {
     const handleSearchChange = (event) => {
-        onSearch(event.target.value); // Call the onSearch callback with the input value
+        onSearch(event.target.value); // Call the onSearch callback with the input val
     };
 
 
@@ -9,8 +9,7 @@ export default function Navbar({ onOpen, onSearch}) {
         <>
         <div className="navbar bg-base-100 p-4 mt-3">
         <div className="navbar-start">
-           
-            <a className="btn btn-ghost text-3xl ml-5 text-white">Clients</a>
+              <a className="btn btn-ghost text-3xl ml-5 text-white">Clients</a>
         </div>
         <div className="navbar-center">
             <div className="form-control">
